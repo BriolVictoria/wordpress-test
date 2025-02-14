432c01cd1265c78981920cae23e39549d7e1b001
BriolVictoria <victoria.briol@student.hepl.be>
2025-02-14 09:32:12 +0100

    Added first iteration example of "the loop"

--- wp-content/themes/dw/index.php
+++ wp-content/themes/dw/index.php
@@ -5,7 +5,27 @@
     <title><?= wp_title('·', false, 'right') . get_bloginfo('name') ?></title>
 </head>
 <body>
+
+    <header>
     <h1><?= get_bloginfo('name') ?></h1>
     <p> <?= get_bloginfo('description') ?></p>
+    </header>
+
+    <main>
+        <?php
+        // On ouvre "la boucle" (The loop), la structure de contrôle de contenu propre à WordPress:
+        if(have_posts()): while (have_posts()): the_post(); ?>
+
+        <h2><?= get_the_title(); ?></h2>
+
+        <div><?= get_the_content(); ?></div>
+
+        <?php
+        // On ferme "la boucle" (The loop)
+        endwhile; else: ?>
+        <p>La page est vide.</p>
+        <?php endif; ?>
+    </main>
+
 </body>
 </html>
