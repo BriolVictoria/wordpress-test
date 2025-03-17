c43c18c0b979d32d725dcf6ddcbace56d23bb33a
BriolVictoria <victoria.briol@student.hepl.be>
2025-03-17 15:01:52 +0100

    Added modifications with DJ

--- wp-content/themes/dw/functions.php
+++ wp-content/themes/dw/functions.php
@@ -1,4 +1,28 @@
 <?php
+// Charger les champs ACF exportés :
+include_once('fields.php');
+
+function hepl_trad_load_textdomain(): void
+{
+    load_theme_textdomain('hepl-trad', get_template_directory() . '/locales');
+}
+
+add_action('after_setup_theme', 'hepl_trad_load_textdomain');
+
+function __hepl(string $translation, array $replacements = [])
+{
+// 1. Récupérer la traduction de la phrase présente dans $translation
+    $base = __($translation, 'hepl-trad');
+
+// 2. Remplacer toutes les occurrences des variables par leur valeur
+    foreach ($replacements as $key => $value) {
+        $variable = ':' . $key;
+        $base = str_replace($variable, $value, $base);
+    }
+
+// 3. Retourner la traduction complète.
+    return $base;
+}
 
 // Gutenberg est le nouvelle éditeur de contenu propre à wordPress
 // il ne nous intérresse pas pour l'utilisation du thème que
--- wp-content/themes/dw/header.php
+++ wp-content/themes/dw/header.php
@@ -11,7 +11,7 @@
     <p> <?= get_bloginfo('description') ?></p>
 
     <nav class="nav">
-        <h2 class="sro">Navigation principale</h2>
+        <h2 class="sro"><?=__hepl('Navigation principale')?></h2>
         <ul class="nav_container">
             <?php foreach (dw_get_navigation_links('header') as $link): ?>
             <li class="nav_item nav_item--<?= $link->icon; ?>">
@@ -19,6 +19,17 @@
             </li>
             <?php endforeach; ?>
         </ul>
+
+        <div class="languages">
+            <ul class="languages__container">
+                <?php foreach (pll_the_languages(['raw' => true]) as $lang): ?>
+                    <li class="languages__item<?= $lang['current_lang'] ? ' languages__item--current' : '' ?>">
+                        <a href="<?= $lang['url'] ?>" lang="<?= $lang['locale'] ?>" hreflang="<?= $lang['locale'] ?>"
+                           class="languages__link"><?= $lang['name'] ?></a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
     </nav>
 </header>
 
