6c713457a00e3f2b0d60d7774cbb09a3e4046694
BriolVictoria <victoria.briol@student.hepl.be>
2025-03-21 11:26:54 +0100

    Added form contact

--- wp-content/themes/dw/functions.php
+++ wp-content/themes/dw/functions.php
@@ -2,6 +2,11 @@
 // Charger les champs ACF exportés :
 include_once('fields.php');
 
+// Vérifier si la session est active ("started") ?
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
 function hepl_trad_load_textdomain(): void
 {
     load_theme_textdomain('hepl-trad', get_template_directory() . '/locales');
@@ -184,11 +189,26 @@ add_action('acf/init', 'create_site_options_page');
 
 // Ajouter un post pour un formulaire de contact personnalisé
 
-add_action('admin_post_nopriv_dw_submit_contact_form', 'dw_handle_contact_form');
 add_action('admin_post_dw_submit_contact_form', 'dw_handle_contact_form');
+add_action('admin_post_nopriv_dw_submit_contact_form', 'dw_handle_contact_form');
+// Chargement de notre classe qui va gérer ce formulaire
+
+require_once(__DIR__ . '/forms/ContactForm.php');
 
 
 function dw_handle_contact_form()
 {
-    var_dump($_POST); die();
+    $form = (new \DW_Theme\Forms\ContactForm())
+        ->rule('firstname', 'required')
+        ->rule('lastname', 'required')
+        ->rule('email', 'required')
+        ->rule('email', 'email')
+        ->rule('message', 'required')
+        ->rule('message', 'no_test')
+        ->sanitize('firstname', 'sanitize_text_field')
+        ->sanitize('lastname', 'sanitize_text_field')
+        ->sanitize('email', 'sanitize_text_field')
+        ->sanitize('message', 'sanitize_textarea_field');
+
+    return $form->handle($_POST);
 }
